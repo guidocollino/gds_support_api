0e97928c552090b82ec89e2665050ff8195da1d3
Guido Collino <guido.collino@aerolaplata.com.ar>
2015-06-29 12:08:17 -0300

    Quitar mongodb logger

---
+++ config/initializers/mongodb_logger.rb
@@ -0,0 +1,6 @@
+# SACAR
+# require 'mongodb_logger/server' # required
+# # this secure you web interface by basic auth, but you can skip this, if you no need this
+# MongodbLogger::Server.use Rack::Auth::Basic do |username, password|
+#   [username, password] == ['admin', 'letitbe']
+# end
